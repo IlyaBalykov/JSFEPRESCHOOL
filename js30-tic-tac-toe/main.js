f4c9787e7b7e80126ae8a4f7f75865275cc29743
IlyaBalykov <balykov.ilya.88@gmail.com>
2022-02-20 23:23:04 +0300

    feat: add records show

--- js30-tic-tac-toe/main.js
+++ js30-tic-tac-toe/main.js
@@ -1,23 +1,44 @@
 const area = document.getElementById('area')
 const modalContainer = document.getElementById('modal-container')
-const modalOverlay= document.getElementById('overlay')
-const modalBtnClose= document.querySelector('.modal__btn-close')
-
+const modalOverlay = document.getElementById('overlay')
+const modalBtnClose = document.querySelector('.modal__btn-close')
+const modalRecords = document.querySelector('.modal__records')
+let arr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 const modalContent = document.getElementById('modal__content')
 
-let isWinner = ''
+let isWinner = '';
 let move = 0;
+let isXSteps = 0;
+let isOSteps = 0;
+window.onload = function () {
+  console.log(localStorage.result)
+  if(localStorage.length > 0) {
+    arr = localStorage.result.split(',')
+  }
+  console.log('теперь' + arr)
+}
+
 
 area.addEventListener('click', event => {
-  if (event.target.className === 'area__box') {
-    move % 2 === 0 ? event.target.innerHTML = 'X' : event.target.innerHTML = 'O';
-    move++
-    check()
+  if (!event.target.innerHTML) {
+    if (event.target.className === 'area__box') {
+      if (move % 2 === 0) {
+        event.target.innerHTML = 'X'
+        move++
+        isXSteps++
+      } else {
+        event.target.innerHTML = 'O'
+        move++
+        isOSteps++
+      }
+      check()
+    }
   }
 })
 modalBtnClose.addEventListener('click', closeModal)
 
 function check() {
+
   const areaBoxes = document.getElementsByClassName("area__box")
   const arr = [
     [0, 1, 2],
@@ -34,21 +55,41 @@ function check() {
       areaBoxes[arr[index][0]].innerHTML === 'X' && areaBoxes[arr[index][1]].innerHTML === 'X' && areaBoxes[arr[index][2]].innerHTML === 'X'
     ) {
       isWinner = 'крестики'
-      showWinner(isWinner)
+      showWinner(isWinner, isXSteps)
     } else if (
       areaBoxes[arr[index][0]].innerHTML === 'O' && areaBoxes[arr[index][1]].innerHTML === 'O' && areaBoxes[arr[index][2]].innerHTML === 'O'
     ) {
       isWinner = 'нолики'
-      showWinner(isWinner)
+      showWinner(isWinner, isOSteps)
     }
   })
 }
 
-function showWinner(winner) {
-  modalContent.innerHTML = `Выиграли ${isWinner}!`
+function showWinner(winner, steps) {
+  modalContent.innerHTML = `Выиграли ${winner} за ${steps} хода!`
   modalContainer.style.display = 'block'
+  setLocalStorage(steps)
+  showResults(steps)
 }
-function closeModal () {
+
+function closeModal() {
   modalContainer.style.display = 'none'
   location.reload()
+}
+
+function setLocalStorage(steps) {
+  arr.pop()
+  arr.unshift(steps)
+  localStorage.setItem('result', arr)
+}
+
+function showResults(steps) {
+
+  arr.forEach((element, index) => {
+    let result = document.createElement('span')
+    result.className = 'result'
+    console.log(element)
+    result.innerHTML = `${element}`
+    modalRecords.appendChild(result)
+  })
 }
